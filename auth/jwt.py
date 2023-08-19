10f72e6b55b82d8bdc12ef437a7f8e21f88581cf
danieltonad <soladan2000@gmail.com>
2023-08-19 12:13:01 -0700

    jwt-issh

--- auth/jwt.py
+++ auth/jwt.py
@@ -1,4 +1,5 @@
 import jwt
+from fastapi import HTTPException, Header
 from dotenv import load_dotenv
 import os
 from time import time
@@ -8,16 +9,28 @@ load_dotenv()
 JWT_SECRET = os.environ.get('JWT_SECRET')
 JWT_ALGORITHM = os.environ.get('JWT_ALGORITHM')
 
-print(JWT_ALGORITHM)
 def token_response(token: str):
     return {
         'access_token': token
     }
 
-def signJWT(userID: str):
+def signJWT(user: str):
     payload = {
-        'userID': userID,
+        'user': user,
         'expiry': time() + 1200
     }
     token = jwt.encode(payload, JWT_SECRET, JWT_ALGORITHM)
     return token_response(token)
+
+def decodeJWT(token: str):
+    try:
+        decoded_token = jwt.decode(token, JWT_SECRET, JWT_ALGORITHM)
+        print(decoded_token)
+        # if decoded_token:
+        #     def getUser():
+        #         return decoded_token['user']
+        return True
+        # return True if decoded_token['expires'] >= time() else HTTPException(status_code=401, detail="Unathorized")
+
+    except:
+        return HTTPException(status_code=500, detail="Token Error", headers=Header())
--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from fastapi import FastAPI
 from request.create_user_request import createUserRequest
-from auth.jwt import signJWT
+from auth.jwt import signJWT, decodeJWT
 app = FastAPI()
 
 
 @app.post('/add_user', tags=['User Registrartion'])
 def save_user(user: createUserRequest):
     user = dict(user)
-    
-    return signJWT('a0rCC7kmhPUM_Ytmy34EmXSZnLoqr3XuBjphgxi6LL2rC')
+    return decodeJWT(signJWT('100'))
