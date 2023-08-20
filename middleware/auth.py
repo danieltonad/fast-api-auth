462910a74167688a37f5d44f9e99b3104805950a
danieltonad <soladan2000@gmail.com>
2023-08-20 16:41:55 -0700

    understood middleware

--- middleware/auth.py
+++ middleware/auth.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Request
 
 async def custom_middleware(request: Request, call_next):
-    print('before Req')
+    print(f'before Req->{request.url.path}')
     response = await call_next(request)
     print('After Req')
     return response
