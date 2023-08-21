d41fd1d1805c68b0fe449de6d2bfa45690b7b8f4
danieltonad <soladan2000@gmail.com>
2023-08-21 06:54:56 -0700

    login route dobne

--- controller/user_controller.py
+++ controller/user_controller.py
@@ -2,11 +2,15 @@ from request.create_user_request import createUserRequest, userLoginRequest
 import uuid
 from pydantic import EmailStr
 from config.db import database
-from schema.user_schema import users_serializer
+from schema.user_schema import users_serializer, user_serializer
 from utility.hash import hash_pwd
+from auth.jwt import signJWT
 
 def login_user(user: userLoginRequest):
-    return 100
+    find = user_serializer(database.fetch({'email': user.email}, limit=1)._items[0])
+    if find['password'] == hash_pwd(user.password):
+        return signJWT(find)
+    return {'details': 'invalid user credentials'}
 
 
 def create_user(user: createUserRequest):
@@ -20,13 +24,8 @@ def create_user(user: createUserRequest):
         return {'detals': 'Email Already Exist!'}
 
 def checkEmailDuplicate(email: EmailStr):
-    users = getUsers()
-    if users:
-        for _ in users:
-            if _['email'] == email:
-                return True
-    return False
-
+    _ = database.fetch({'email': email})._items
+    return True if len(_) > 0 else False
 def getUsers() -> []:
     data = []
     prev = database.fetch()
--- routes/user.py
+++ routes/user.py
@@ -1,15 +1,14 @@
 from fastapi import APIRouter
 from request.create_user_request import createUserRequest, userLoginRequest
 from controller.user_controller import create_user, login_user
-from auth.jwt import signJWT, decodeJWT
 
 user = APIRouter()
 
 
-@user.post('/add_user', tags=['User Registrartion'])
+@user.post('/register', tags=['User Registrartion'])
 def save_user(user: createUserRequest):
     return create_user(user)
 
-@user.post('/login_user')
+@user.post('/login', tags=['User Login'])
 def user_login(user: userLoginRequest):
     return login_user(user)
