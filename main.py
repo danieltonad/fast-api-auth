532f1520c3d0277c3e9fc7ab7e288ee52bd027e8
danieltonad <soladan2000@gmail.com>
2023-09-14 18:17:24 -0700

    enable cors

--- main.py
+++ main.py
@@ -3,9 +3,18 @@ from request.user_request import createUserRequest
 from auth.jwt import signJWT, decodeJWT
 from middleware.auth import custom_middleware
 from routes.user import user
-import hashlib
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# Configure CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods
+    allow_headers=["*"],  # Allow all HTTP headers
+)
 
 app.include_router(user, prefix='/user')
 
